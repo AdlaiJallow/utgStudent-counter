00dc29cc87581f0126c7df64234f944d2644bf8f
AdlaiJallow <94768418+AdlaiJallow@users.noreply.github.com>
2023-12-31 02:59:56 +0000

    Done and dusted

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let count = 0;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+function increment(){
+  count += 1;
+  countEl.innerText = count;
+  console.log(count);
+}
+
+
+// 1. Create a function, save(), which logs out the count when it's called
+function save() {
+  let countDash = count + " - ";
+  saveEl.textContent += countDash;
+
+  countEl.textContent = 0;
+  count = 0;
+}
